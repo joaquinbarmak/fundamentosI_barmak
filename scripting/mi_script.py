57743cbb8a2b7176721c4b8c83370baba9cd801e
joaquinbarmak <joaquinbarmak1@gmail.com>
2023-04-02 22:52:20 -0300

    ejs

--- scripting/mi_script.py
+++ scripting/mi_script.py
@@ -31,16 +31,80 @@ def dia_de_la_semana_favorito(dia):
 
 #print(dia_de_la_semana_favorito('Sabado'))
 
+#ej 6
+def rango_radio (longitud):
+    return 223.0 <= longitud <= 586.8
+
+#print(rango_radio(100))
+
+#ej 7
+
+def rango_radio_r (longitud):
+    return 223.0 <= longitud <= 586.8 and longitud != 314.5
+
+#print(rango_radio_r(314.6))
+
+#ej 8
+def tiene_descuento (edad):
+    return edad <= 12 or edad >= 60
+
+#print(tiene_descuento(70))
+
+#ej 9
+def xor (booleano1, booleano2):
+    return booleano1 != booleano2
+
+#print(xor(False, False))
+
+#ej 10
+def saludo (nombre, apellido):
+    return "Hola " + nombre + ", en que andan los " + apellido + "?"
+
+#print(saludo("Marcela", 'Roitman'))
+
 #ej 11
 
 def empieza_con_h (p):
     if p.startswith('h'):
         return len(p)
 
-print(empieza_con_h('zapatilla'))
+#print(empieza_con_h('zapatilla'))
 
 #ej 12
 def buenos(f):
     return f.startswith("Buenos") or f.startswith("Buenas")
 
-print(buenos('Buenos dias'))
+#print(buenos('Buenos dias'))
+
+#ej 13
+def es_multiplo (num1, num2):
+    return num2 % num1 == 0
+
+#print(es_multiplo(3, 10))
+
+#ej 14
+def par_impar_cero (num):
+    if num % 2 == 0 and num != 0:
+        return "es par"
+    elif num % 2 != 0:
+        return "es impar"
+    else:
+        return "es cero"
+
+#print(par_impar_cero(0))
+
+#ej 15
+def a_en_frase (frase):
+    ases = 0
+    for letra in frase:
+        if letra == "a" or letra == "A":
+            ases += 1
+    return ases
+
+#print(a_en_frase('hasta la vista bAby'))
+
+#ej16
+def cuantos_meses_me_quedan_de_vida (dinero):
+    return dinero / 60000
+
+print(cuantos_meses_me_quedan_de_vida(120000))
