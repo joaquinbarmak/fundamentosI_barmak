9d0fd689dbb2185d593a9b5aec68302ff398fc6d
joaquinbarmak <joaquinbarmak1@gmail.com>
2023-05-15 09:51:10 -0300

    web

--- scripting/web/web/clases/primer_clase_web.py
+++ scripting/web/web/clases/primer_clase_web.py
@@ -4,4 +4,16 @@ Internet es la conexion cableada entre maquinas en el mundo.
 
 La web es esa informacion que circula entre las maquinas.
 
+Cliente y servidor son computadoras, y nosotros funcionamos como clientes (nuestra maquina).
+
+El servidor aporta algo, mientras que el cliente es el que consume.
+Siempre va a haber un pedido del cliente hacia el servidor.
+Cliente pide, servidor responde.
+
+Contenido: datos, contenido, ...
+
+Backend: servidor
+
+Frontend: cliente 
+
 '''
