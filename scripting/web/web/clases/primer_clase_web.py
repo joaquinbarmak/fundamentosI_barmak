cdaecadca63c5d3cc89a7a63da41b0f1c8a2d7a7
joaquinbarmak <joaquinbarmak1@gmail.com>
2023-05-22 10:16:31 -0300

    cambios

--- scripting/web/web/clases/primer_clase_web.py
+++ scripting/web/web/clases/primer_clase_web.py
@@ -9,6 +9,7 @@ Cliente y servidor son computadoras, y nosotros funcionamos como clientes (nuest
 El servidor aporta algo, mientras que el cliente es el que consume.
 Siempre va a haber un pedido del cliente hacia el servidor.
 Cliente pide, servidor responde.
+El servidor no debe, o no deberia, hacer pedidos al cliente.
 
 Contenido: datos, contenido, ...
 
@@ -16,4 +17,57 @@ Backend: servidor
 
 Frontend: cliente 
 
+Protocolo: un sistema de reglas que permiten que dos o mas entidades de un sistema de comunicacion 
+           se comuniquen entre ellas para transmitir informacion.
 '''
+'''
+Web app: - persiste (almacena, guarda) informacion.
+         - tiene toda la logica que me permite a mi procesar y manejar la informacion.
+
+Pagina Web: - no almacena informacion.
+            - un archivo HTML expuesto en la nube.
+
+Diferencia entre sitio y pagina web: un sitio son muchos archivos HTML interconectados.
+
+Aplicacion Rest : cada recurso se va a corresponder con un url en particular.
+Recurso: un tipo de item de la base de datos
+
+'''
+'''
+Request --> un pedido a un servidor.
+
+Verbos http --> disparan acciones particulares --> siempre hablando de aplicaciones Rest.
+
+Post: verbo http asociado a persistir (guardar) datos de cero.
+
+get: para obtener el contenido de una web o para realizar una peticion a un API.
+
+delete: verbo asociado a borrar datos.
+
+patch: verbo asociado a reescribir datos.
+
+'''
+'''
+
+htts:// --> el protocolo que utilizamos para comunicarnos por la red
+dominio --> el nombre con el cual voy a mapear una IP particular. Sirve como un alias del IP. (www.mercadolibre.com)
+recurso --> las cosas que puedo consultar de la base de datos. (/aros-de-plata)
+
+las URLs mapean recursos
+
+diferencia entre path y url : una accede a datos mediante el internet (url), y a la otra de forma local (path).
+
+'''
+
+
+import requests # para ejecutar todo lo que es requests hay que usar python3 en la terminal
+
+respuesta = requests.get('https://api.github.com/users/ajvelezrueda/orgs')
+datos = respuesta.json()
+# en cuantas organizaciones esta involucrado el usuario:
+print(len(datos))
+# en login sacamos la informacion del nombre del usuario:
+print(len(datos[0]['login']))
+#lista de nombres de las organizaciones en las que esta involucrada [TAREA]:
+print(respuesta)
+print(respuesta.headers)
---
+++ scripting/web/web/clases/segunda_clase_web.py
@@ -0,0 +1,42 @@
+import requests
+
+respuesta = requests.get('https://pokeapi.co/api/v2/pokemon/ditto')
+datos = respuesta.json()
+
+'''
+
+1) describir las partes de la url
+2) que respuesta obtenes al hacer las get a esa url?
+3) cual es el content type de esa respuesta?
+4) cual es el status code de la respuesta?
+5) cuantas habilidades (habilities) tiene este pokemon?
+
+'''
+
+# 1) pokeapi es la direccion, y despues se va esecificando en secciones cada vez mas especificas
+
+# 2) Toda la api, con todos sus datos.
+
+#print(datos) --> todos los datos en bruto
+#print(datos.keys()) # --> los titulos de los datos que me va a dar
+
+
+# 3) application/json; charset=utf-8 --> el tipo de dato es lo que esta despues de la barra (json)
+
+#print(respuesta.headers['Content-Type'])
+
+# 4) Status Code = 200
+
+#print(respuesta.status_code)
+
+# 5) Abilidades = 2
+
+#print(len(datos['abilities']))
+
+
+
+
+respuesta2 = requests.get('https://pokeapi.co/api/v2/ability/150/')
+datos2 = respuesta2.json()
+
+print(datos2['effect_entries'])
